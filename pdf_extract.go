5d1785aad0bfcc33c6f615e9b1c8d557da0fb273
DESKTOP-F6Q3OLD\Peter's PC <peter.timperman@hope.edu>
2025-09-03 20:16:00 -0400

    got the emf title finder working

--- pdf_extract.go
+++ pdf_extract.go
@@ -8,7 +8,10 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
+	"sort"
 	"strings"
+	"unicode"
 )
 
 // type VObject struct {
@@ -22,6 +25,11 @@ type EmbeddedObjectPaths struct {
 	pdfPath string
 }
 const ADOBE_TYPE_PROG_ID = "Acrobat.Document.DC"
+var (
+	pdfExtPattern1 = regexp.MustCompile("\x2e\x00\x70\x00\x64\x00\x66\x00") 
+	pdfExtPattern2 = regexp.MustCompile("\x2e\x00\x50\x00\x44\x00\x46\x00\x00")
+)
+
 
 func findEmbeddedObjects(documentXMLFile io.Reader) []EmbeddedObjectRefs {
 	documentDecoder := xml.NewDecoder(documentXMLFile)
@@ -44,14 +52,15 @@ func findEmbeddedObjects(documentXMLFile io.Reader) []EmbeddedObjectRefs {
 				switch localName {
 				case "OLEObject":
 					tempPdfID := ""
+					isPdf := false
 					for _, attr := range element.Attr {
 						if attr.Name.Local == "ProgID" && attr.Value == ADOBE_TYPE_PROG_ID {
-							currentEmbeddedObject.pdfID = attr.Value
+							isPdf = true
 						} else if attr.Name.Local == "id" {
-							currentEmbeddedObject.pdfID = attr.Value
+							tempPdfID = attr.Value
 						}
 					}
-					if tempPdfID != "" {
+					if tempPdfID != "" && isPdf {
 						currentEmbeddedObject.pdfID = tempPdfID
 					}
 
@@ -76,9 +85,81 @@ func findEmbeddedObjects(documentXMLFile io.Reader) []EmbeddedObjectRefs {
 	return embeddedObjectRefs
 }
 
-// func findTitleInEmf(emfFile io.Reader) string {
+func findTitleInEmf(emfFile io.Reader) string {
+	// Title is stored in the last 300 bytes of the file with a specific pattern
 
-// }
+	data , err := io.ReadAll(emfFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := data[len(data)-300:]
+	
+	// Search for the PDF extension pattern
+	match1 := pdfExtPattern1.FindAllIndex(content, -1)
+	match2 := pdfExtPattern2.FindAllIndex(content, -1)
+	// fmt.Println(match1)
+	// fmt.Println(match2)
+	var loc1 []int
+	var loc2 []int
+	if len(match1) == 0 && len(match2) == 0 {
+		// fmt.Println("No PDF extension pattern found in the last 300 bytes of the EMF file")
+		return ""
+	}
+	if len(match1) > 0 {
+		loc1 = match1[len(match1)-1]
+	}else {
+		loc1 = nil
+	}
+	if len(match2) > 0 {
+		loc2 = match2[len(match2)-1]
+	} else {
+		loc2 = nil
+	}
+
+	if loc1 == nil && loc2 == nil {
+		fmt.Println("No PDF extension pattern found in the last 300 bytes of the EMF file")
+		return ""
+	}
+	var loc []int
+	if loc1 != nil {
+		loc = loc1
+	} else {
+		loc = loc2
+	}
+	// fmt.Println("Found PDF extension pattern at index:", loc[0])
+	var titleCharInts []int
+	startIndex := loc[0] -1 
+	nullByteCounter := 0
+	index := startIndex 
+	for  index > 0 && nullByteCounter < 3 && (len(titleCharInts) < 30) {
+		char := content[index]
+		if char != 0x00 { 
+			// fmt.Printf("Found title character byte: 0x%02x at index %d\n", char, index)
+			titleCharInts = append([]int{int(char)}, titleCharInts...)
+			nullByteCounter = 0
+		} else {
+			// fmt.Printf("Found null byte at index %d counter is %d \n", index, nullByteCounter)
+			nullByteCounter++
+		}
+		index-- 
+	}
+	// Reverse the titleCharInts
+	// fmt.Println("Extracted title character bytes (reversed):", titleCharInts)
+	sort.Slice(titleCharInts, func(i, j int) bool { return i < j })
+	// Convert to runes
+	titleRunes := make([]rune, len(titleCharInts))
+	for i, charInt := range titleCharInts {
+		titleRunes[i] = rune(charInt)
+	}
+	// Remove non-printable characters from the title
+	titleRunes = []rune(strings.TrimFunc(string(titleRunes), func(r rune) bool {
+		if (!unicode.IsPrint(r) && r != '\n' && r != '\r' && r != '\t' && r != '.'){
+			return true
+		}
+		return false
+	}))
+	return string(titleRunes)	
+}
 
 func getRelToPaths(relXMLFile io.Reader) map[string]string {
 	relToPathMap := make(map[string]string)
@@ -97,7 +178,7 @@ func getRelToPaths(relXMLFile io.Reader) map[string]string {
 					id = attr.Value
 				} else if attr.Name.Local == "Target" {
 					if (strings.HasPrefix(attr.Value, "media/") || strings.HasPrefix(attr.Value, "embeddings/")) && (strings.HasSuffix(attr.Value, ".bin") || strings.HasSuffix(attr.Value, ".emf")) {
-						target = attr.Value
+						target = "word/" + attr.Value
 					}
 				}
 			}
@@ -109,25 +190,25 @@ func getRelToPaths(relXMLFile io.Reader) map[string]string {
 	return relToPathMap
 }
 
-func getFileNameFromEmfPath(emfPath string) string {
-	emfFile , err := os.Open(emfPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer emfFile.Close()
-	// Get the last 300 bytes of the file
-	stat, err := file.Stat()
-    if err != nil {
-        return nil, err
-    }
+// func getFileNameFromEmfPath(emfPath string) string {
+// 	emfFile , err := os.Open(emfPath)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer emfFile.Close()
+// 	// Get the last 300 bytes of the file
+// 	stat, err := file.Stat()
+//     if err != nil {
+//         return nil, err
+//     }
 
-    fileSize := stat.Size()
-    if fileSize <= n {
-        // If file is smaller than n bytes, read entire file
-        return io.ReadAll(file)
-    }
-	return ""
-}
+//     fileSize := stat.Size()
+//     if fileSize <= n {
+//         // If file is smaller than n bytes, read entire file
+//         return io.ReadAll(file)
+//     }
+// 	return ""
+// }
 
 func main() {
 	var verbose bool
@@ -173,6 +254,17 @@ func main() {
 		if emfOk && pdfOk {
 			embeddedObjectPaths = append(embeddedObjectPaths, EmbeddedObjectPaths{emfPath: emfPath, pdfPath: pdfPath})
 			fmt.Printf("Found embedded object paths: EMF: %s, PDF: %s\n", emfPath, pdfPath)
+			// Extract the title from the emf file
+
+			emfContentFile, err := wordDirReader.Open(emfPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer emfContentFile.Close()
+			title := findTitleInEmf(emfContentFile)
+			if title != "" {
+				fmt.Printf("Extracted title: %s\n", title)
+			} 
 		}
 	}
 }
